c548ce2e3889c2ac1c504391935c31391a915130
aleczhoushuang <75024748+aleczhoushuang@users.noreply.github.com>
2022-07-14 15:28:57 +0200

    update backend

--- config/db.config.js
+++ config/db.config.js
@@ -5,8 +5,9 @@ module.exports = {
   PASSWORD: "8ajk2qdv8ajk2qdv",
   DB: "shotgun",
   dialect: "mysql",
+  timeout: 10000,
   pool: {
-    max: 5,
+    max: 100,
     min: 0,
     acquire: 30000,
     idle: 10000
--- models/db.js
+++ models/db.js
@@ -1,7 +1,26 @@
+
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
-const connection = mysql.createConnection({
+//const connection = mysql.createConnection({
+//  host: dbConfig.HOST,
+//  port: dbConfig.PORT,
+//  user: dbConfig.USER,
+//  password: dbConfig.PASSWORD,
+//  database: dbConfig.DB
+//});
+
+// open the MySQL connection
+//connection.connect(error => {
+//  if (error) throw error;
+//  console.log("Successfully connected to the database.");
+//});
+
+const connection = mysql.createPool({
+  connectionLimit : dbConfig.pool.max,
+  acquireTimeout: dbConfig.pool.acquire,
+  connectTimeout: dbConfig.timeout,
+  supportBigNumbers: true,
   host: dbConfig.HOST,
   port: dbConfig.PORT,
   user: dbConfig.USER,
@@ -9,10 +28,12 @@ const connection = mysql.createConnection({
   database: dbConfig.DB
 });
 
-// open the MySQL connection
-connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) {
+           console.error('error connecting: ' + error.stack);
+           throw error;
+  }
+  console.log('resulat=' + results[0].solution + '. Successfully connected to the database.');
 });
 
 module.exports = connection;
